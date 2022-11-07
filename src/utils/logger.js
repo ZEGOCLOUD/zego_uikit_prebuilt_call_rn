0c24f2da3050c07a709cf871ee8de054e58b0d5b
lihao <lihao@zego.im>
2022-11-07 21:45:39 +0800

    🐛 Fixed log bug

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,10 @@
 export const zloginfo = (...msg) => {
-    console.log("ZEGOUIKit[INFO]: ", ...msg);
-}
-export const zlogwarning = (msg) => {
-    console.warn("ZEGOUIKit[WARNING]: ", ...msg);
-}
+  console.log('ZEGOUIKit[INFO]: ', ...msg);
+};
+export const zlogwarning = (...msg) => {
+  console.warn('ZEGOUIKit[WARNING]: ', ...msg);
+};
 
-export const zlogerror = (msg) => {
-    console.error("ZEGOUIKit[ERROR]: ", ...msg);
-}
+export const zlogerror = (...msg) => {
+  console.error('ZEGOUIKit[ERROR]: ', ...msg);
+};
