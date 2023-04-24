4e90b4a82a16cc70b067c26a9b221ef582c0f7ca
lihao <lihao@zego.im>
2023-04-24 20:06:23 +0800

    Adapt the new plug-in incoming mode

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/callInvitation/CallWithInvitation';
+// import App from './src/call/AppNavigation';
 import { name as appName } from './app.json';
-import ZegoUIKit from '@zegocloud/zego-uikit-rn';
 import ZegoUIKitPrebuiltCallService from '@zegocloud/zego-uikit-prebuilt-call-rn';
 import ZegoUIKitSignalingPlugin from './src/callInvitation/plugin';
 
