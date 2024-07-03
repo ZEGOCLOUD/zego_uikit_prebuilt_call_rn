9998babe73278e31a1d20f13c23955bae130b201
Kael Ding <415676243@qq.com>
2024-07-03 17:28:12 +0800

    Adjust position of duration label.

--- src/call/ZegoPrebuiltForegroundView.js
+++ src/call/ZegoPrebuiltForegroundView.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         position: 'absolute',
         width: '100%',
         height: '100%',
-        top: 6,
+        top: 26,
         zIndex: 5,
         alignItems: 'center',
     },
