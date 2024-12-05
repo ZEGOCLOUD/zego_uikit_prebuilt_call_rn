400a3b16ba8106989e699ed7abcf8f34328d03a6
tripli <tripli@zego.im>
2024-12-05 17:14:28 +0800

    Fix the error that occurs when react-native-screens is executed for the first time on certain devices.

--- example/android/app/src/main/java/com/zegocloud/zegouikit/example/prebuiltcall/MainActivity.kt
+++ example/android/app/src/main/java/com/zegocloud/zegouikit/example/prebuiltcall/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.zegocloud.zegouikit.example.prebuiltcall
 
+import android.os.Bundle
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
- 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Prevent the react-native-screens component from throwing an error on first run: calling Fragment constructor caused an exception.
+    super.onCreate(null)
+  }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "ZegoPrebuiltCall"
- 
+
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
