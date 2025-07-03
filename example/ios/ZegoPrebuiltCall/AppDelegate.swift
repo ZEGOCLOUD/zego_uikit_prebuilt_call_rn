cf486b28815c8abdd27065f7442e9ffc96b8a8d1
tripli <tripli@zego.im>
2025-07-03 17:28:18 +0800

    Fix the issue of ZPNs code crashing on iOS during execution from React Native 0.77.

--- example/ios/ZegoPrebuiltCall/AppDelegate.swift
+++ example/ios/ZegoPrebuiltCall/AppDelegate.swift
@@ -4,6 +4,7 @@ import React_RCTAppDelegate
 import ReactAppDependencyProvider
 
 @main
+@objc(AppDelegate)
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     self.moduleName = "ZegoPrebuiltCall"
